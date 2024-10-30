db915c00809468af2889e8e74262c0e57318d750
justin-0 <itsjustinoconnell@gmail.com>
2024-10-30 22:53:03 +0000

    hero radial gradient

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,8 +2,9 @@ import ArrowIcon from "@/assets/icons/arrow-w.svg";
 
 export const Hero = () => {
   return (
-    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
-      <div className="container">
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] relative overflow-clip">
+      <div className="absolute left-1/2 -translate-x-1/2 top-[calc(100%-96px)] h-[375px] w-[750px] rounded-[100%]  border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)]"></div>
+      <div className="container relative">
         <div className="flex items-center justify-center">
           <a
             href="#"
