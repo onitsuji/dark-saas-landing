f6b80f03495bb5a9a03a58804d374c72bbf199f1
justin-0 <itsjustinoconnell@gmail.com>
2024-10-30 14:38:30 +0000

    hero component initial

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,3 +1,20 @@
 export const Hero = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
+      <div className="container">
+        <div className="flex items-center justify-center">
+          <a href="#">
+            <span>Version 2.0 is here</span>
+            <span>Read more</span>
+          </a>
+        </div>
+        <h1>One Task at a Time</h1>
+        <p>
+          Celebrate your accomplishments with an app designed to track, motivate
+          and celebrate success.
+        </p>
+        <button>Get for free</button>
+      </div>
+    </div>
+  );
 };
