ca2443d27ae6513d41346850d1c75d5d65034ec4
justin-0 <itsjustinoconnell@gmail.com>
2024-10-30 14:47:38 +0000

    hero styling

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,8 +3,13 @@ export const Hero = () => {
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
       <div className="container">
         <div className="flex items-center justify-center">
-          <a href="#">
-            <span>Version 2.0 is here</span>
+          <a
+            href="#"
+            className="inline-flex gap-x-3 border py-1 px-2 rounded-lg border-white/30"
+          >
+            <span className="bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] text-transparent bg-clip-text [-webkit-background-clip:text]">
+              Version 2.0 is here
+            </span>
             <span>Read more</span>
           </a>
         </div>
