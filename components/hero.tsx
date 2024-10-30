8d46e5195da4c7fb5a73152c2fff7e340506ea45
justin-0 <itsjustinoconnell@gmail.com>
2024-10-30 14:53:17 +0000

    hero content styling

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,3 +1,5 @@
+import ArrowIcon from "@/assets/icons/arrow-w.svg";
+
 export const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px]">
@@ -5,16 +7,20 @@ export const Hero = () => {
         <div className="flex items-center justify-center">
           <a
             href="#"
-            className="inline-flex gap-x-3 border py-1 px-2 rounded-lg border-white/30"
+            className="inline-flex gap-3 border py-1 px-2 rounded-lg border-white/30"
           >
             <span className="bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] text-transparent bg-clip-text [-webkit-background-clip:text]">
               Version 2.0 is here
             </span>
-            <span>Read more</span>
+            <span>
+              Read more <ArrowIcon className="inline-block" />
+            </span>
           </a>
         </div>
-        <h1>One Task at a Time</h1>
-        <p>
+        <h1 className="text-7xl font-bold tracking-tighter text-center mt-8">
+          One Task at a Time
+        </h1>
+        <p className="text-center ">
           Celebrate your accomplishments with an app designed to track, motivate
           and celebrate success.
         </p>
