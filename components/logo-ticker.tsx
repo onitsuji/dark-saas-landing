1af7528d8db901da09a83fa3a0f971b9b539621c
justin-0 <itsjustinoconnell@gmail.com>
2024-11-01 13:07:36 +0000

    before after width

--- components/logo-ticker.tsx
+++ components/logo-ticker.tsx
@@ -19,12 +19,12 @@ const images = [
 
 export const LogoTicker = () => {
   return (
-    <div className="bg-black text-white py-[72px]">
+    <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
         <h2 className="text-center text-xl text-white/70">
           Trusted by world class teams
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-20 after:w-20 relative before:left-0 before:top-0 after:right-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 before:top-0 after:right-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
           <div className="flex gap-16">
             {images.map((img) => (
               <Image
