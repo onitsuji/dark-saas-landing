2af3627f9c78eea7cbf7d4c2b0b6507edc338dc6
justin-0 <itsjustinoconnell@gmail.com>
2024-11-01 15:38:08 +0000

    features cards

--- components/features.tsx
+++ components/features.tsx
@@ -1,3 +1,5 @@
+import EcoSystemIcon from "@/assets/icons/ecosystem.svg";
+
 const features = [
   {
     title: "Integration Ecosystem",
@@ -20,14 +22,22 @@ export const Features = () => {
   return (
     <div className="bg-black text-white">
       <div className="container">
-        <h2>Everything you need</h2>
-        <p>
+        <h2 className="text-center font-bold text-5xl tracking-tighter">
+          Everything you need
+        </h2>
+        <p className="text-center mt-5 text-xl text-white/70">
           Set tasks, get reminders and see your teams progress rise quickly.
         </p>
       </div>
-      <div>
+      <div className="mt-16 flex flex-col gap-6">
         {features.map((feature) => (
-          <div>
+          <div
+            key={feature.title}
+            className="border border-white/30 px-5 py-10 text-center"
+          >
+            <div className="flex justify-center">
+              <EcoSystemIcon />
+            </div>
             <h3>{feature.title}</h3>
             <p>{feature.description}</p>
           </div>
